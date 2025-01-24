d4a37a9c2283fd647fcb84ebcf7cfe99dccb04ac
vinossauro <viniaraujoaraujo.va@gmail.com>
2025-01-23 21:17:25 -0300

    Co-authored-by: João Galetti <galetti909@users.noreply.github.com>

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+	t.title = "Bolho X Estouro"
+	t.console = true
+	t.window.width = 1200
+	t.window.height = 720
+	t.window.vsync = 0
+end
--- main.lua
+++ main.lua
@@ -1,40 +1,22 @@
--- main.lua
-local Player = require("player")
-local Physics = require("physics")
-local Object =  require("object")
-
--- GAME VARIABLES
-local level1 = {}
-local player
-
+local STI = require("sti")
 
 function love.load()
-    player = Player.new({x = 150, y = 150}, 100)
-
-    table.insert(level1, Object.newRectangle(100, 500, 500, 300))
+	Map = STI("map/1.lua", {"box2d"})
+	World = love.physics.newWorld(0,0)
+	Map:box2d_init(World)
+	Map.layers.solid.visible = false
+	--background = love.graphics.newImage("assets/background.png")
 end
 
 function love.update(dt)
-    player:update(dt)
-
-    if Physics.collision({player}, level1) then
-        print("Collision detected!")
-    end
-
+	World:update(dt)
 end
 
 function love.draw()
-    player:draw()
+	--love.graphics.draw(background)
+	Map:draw(0, 0, 2, 2)
+	love.graphics.push()
+	love.graphics.scale(2,2)
 
-    for _, obj in ipairs(level1) do
-        obj.draw(player.position, player.initial_position)
-    end
-end
-
-function love.keypressed(key)
-    player:keypressed(key)
-end
-
-function love.keyreleased(key)
-    player:keyreleased(key)
+	love.graphics.pop()
 end
